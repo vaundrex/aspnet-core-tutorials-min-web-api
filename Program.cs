dbe511b630afeefb7bd2b9a1fb015a2a3db1a643
vaundrex <vaundrex@gmail.com>
2024-03-07 16:59:31 +0000

    completed code

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Query;
 using ToDoApi;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -15,11 +16,43 @@ app.MapGet("/todoitems", async (TodoDb db) =>
 app.MapGet("/todoitems/complete", async (TodoDb db) => 
     await db.Todos.Where(t => t.IsComplete).ToListAsync());
 
-app.MapGet("/todolist/{id}", async Results<Ok<Todo>, NotFound> (int Id, TodoDb db) =>
+app.MapGet("/todoitems/{id}", async Task<Results<Ok<Todo>, NotFound>> (int Id, TodoDb db) =>
     await db.Todos.FindAsync(Id)
         is Todo todo
-            ? Results.Ok(todo)
-            : Results.NotFound()
+            ? TypedResults.Ok(todo)
+            : TypedResults.NotFound()
 );
+
+app.MapPost("/todoitems", async (Todo todo, TodoDb db) =>
+{
+    db.Todos.Add(todo);
+    await db.SaveChangesAsync();
+
+    return Results.Created($"/todoitems/{todo.Id}", todo);
+});
+
+app.MapPut("/todoitems{id}", async (int id, Todo inputTodo, TodoDb db) =>
+{
+    var todo = await db.Todos.FindAsync(id);
+    if (todo is null) return Results.NotFound();
+
+    todo.Name = inputTodo.Name;
+    todo.IsComplete = inputTodo.IsComplete;
+
+    await db.SaveChangesAsync();
+
+    return Results.NoContent();
+});
+
+app.MapDelete("/todoitems/{id}", async Task<Results<NoContent, NotFound>> (int id, TodoDb db) => 
+{
+    if(await db.Todos.FindAsync(id) is Todo todo){
+        db.Todos.Remove(todo);
+        await db.SaveChangesAsync();
+        return TypedResults.NoContent();
+    }
+
+    return TypedResults.NotFound();
+});
 
 app.Run();
