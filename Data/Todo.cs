8a743b9d16df0b48e6ba41edd5bfba24e8456d0d
vaundrex <vaundrex@gmail.com>
2024-03-10 11:32:03 +0000

    DTO

--- Data/Todo.cs
+++ Data/Todo.cs
@@ -5,4 +5,5 @@ public class Todo
     public int Id {get; set; }
     public string? Name { get; set; }
     public bool IsComplete { get; set; }
+    public string? Secret { get; set; }
 }
--- Program.cs
+++ Program.cs
@@ -5,38 +5,49 @@ using ToDoApi;
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<TodoDb>(opt => opt.UseInMemoryDatabase("TodoList"));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+// builder.Services.ConfigureHttpJsonOptions(options => {
+//     options.SerializerOptions.WriteIndented = true;
+//     // options.SerializerOptions.IncludeFields = true;
+// });
 var app = builder.Build();
 
 var todoItems = app.MapGroup("/todoitems");
 
 app.MapGet("/", () => "Welcome to the Todo API");
 
+app.MapGet("/throw", () => new Exception("Sample exception.")); 
+
 todoItems.MapGet("/", GetAllTodos);
 
 todoItems.MapGet("/complete", GetCompleteTodos);
 
-todoItems.MapGet("/{id}", async Task<Results<Ok<Todo>, NotFound>> (int Id, TodoDb db) =>
+todoItems.MapGet("/{id}", async Task<Results<Ok<TodoItemDTO>, NotFound>> (int Id, TodoDb db) =>
     await db.Todos.FindAsync(Id)
         is Todo todo
-            ? TypedResults.Ok(todo)
+            ? TypedResults.Ok(new TodoItemDTO(todo))
             : TypedResults.NotFound()
 );
 
-todoItems.MapPost("/", async Task<IResult> (Todo todo, TodoDb db) =>
+todoItems.MapPost("/", async Task<IResult> (TodoItemDTO todoItemDTO, TodoDb db) =>
 {
+    var todo = new Todo{
+        Name = todoItemDTO.Name,
+        IsComplete = todoItemDTO.IsComplete
+    };
+
     db.Todos.Add(todo);
     await db.SaveChangesAsync();
 
     return Results.Created($"/todoitems/{todo.Id}", todo);
 });
 
-todoItems.MapPut("/{id}", async Task<IResult> (int id, Todo inputTodo, TodoDb db) =>
+todoItems.MapPut("/{id}", async Task<IResult> (int id, TodoItemDTO todoItemDTO, TodoDb db) =>
 {
     var todo = await db.Todos.FindAsync(id);
     if (todo is null) return Results.NotFound();
 
-    todo.Name = inputTodo.Name;
-    todo.IsComplete = inputTodo.IsComplete;
+    todo.Name = todoItemDTO.Name;
+    todo.IsComplete = todoItemDTO.IsComplete;
 
     await db.SaveChangesAsync();
 
@@ -59,10 +70,13 @@ app.Run();
 
 static async Task<IResult> GetAllTodos(TodoDb db)
 {
-    return TypedResults.Ok(await db.Todos.ToArrayAsync());
+    return TypedResults.Ok(await db.Todos.Select(t => new TodoItemDTO(t)).ToArrayAsync());
 }
 
 static async Task<IResult> GetCompleteTodos(TodoDb db)
 {
-    return TypedResults.Ok(await db.Todos.Where(t => t.IsComplete).ToArrayAsync());
+    return TypedResults.Ok(
+        await db.Todos
+        .Where(t => t.IsComplete)
+        .Select(t => new TodoItemDTO(t)).ToArrayAsync());
 }
