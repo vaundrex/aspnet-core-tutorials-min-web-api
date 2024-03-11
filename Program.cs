d552af55fc587fd8faa2d0ce3f3717f0dc09ff6f
Alison <alison@johnston.name>
2024-03-11 08:38:22 +0000

    progress

--- Program.cs
+++ Program.cs
@@ -1,6 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using ToDoApi;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<TodoDb>(opt => opt.UseInMemoryDatabase("TodoList"));
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+
+app.MapGet("/todoitems", async (TodoDb db) =>
+    await db.Todos.Where(t =>t.IsComplete).ToListAsync()
+);
 
 app.Run();
