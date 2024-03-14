8bc3e9b598350fd97e10553f1c22502906ded672
Vaundrex <vaundrex@gmail.com>
2024-03-14 21:11:00 +0000

    Extension methods

---
+++ Endpoints/ToDoEndpoints.cs
@@ -0,0 +1,70 @@
+﻿using System.Text.RegularExpressions;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.EntityFrameworkCore;
+
+namespace ToDoApi;
+
+public static class TodoEndpoints
+{
+    
+    public static RouteGroupBuilder MapTodoApi(this RouteGroupBuilder group)
+    {
+        group.MapGet("/", async Task<IResult> (TodoDb db) => 
+            TypedResults.Ok(await db.Todos.Select(t => new TodoItemDTO(t)).ToArrayAsync())
+        );
+
+        group.MapGet("/complete", async Task<IResult> (TodoDb db) => 
+            TypedResults.Ok(
+                await db.Todos
+                .Where(t => t.IsComplete)
+                .Select(t => new TodoItemDTO(t)).ToArrayAsync())
+        );
+
+        group.MapGet("/{id}", async Task<Results<Ok<TodoItemDTO>, NotFound>> (int Id, TodoDb db) =>
+            await db.Todos.FindAsync(Id)
+                is Todo todo
+                    ? TypedResults.Ok(new TodoItemDTO(todo))
+                    : TypedResults.NotFound()
+        );
+
+        group.MapPost("/", async Task<IResult> (TodoItemDTO todoItemDTO, TodoDb db) =>
+        {
+            var todo = new Todo{
+                Name = todoItemDTO.Name,
+                IsComplete = todoItemDTO.IsComplete
+            };
+
+            db.Todos.Add(todo);
+            await db.SaveChangesAsync();
+
+            return Results.Created($"/todoitems/{todo.Id}", todo);
+        });
+
+        group.MapPut("/{id}", async Task<IResult> (int id, TodoItemDTO todoItemDTO, TodoDb db) =>
+        {
+            var todo = await db.Todos.FindAsync(id);
+            if (todo is null) return Results.NotFound();
+
+            todo.Name = todoItemDTO.Name;
+            todo.IsComplete = todoItemDTO.IsComplete;
+
+            await db.SaveChangesAsync();
+
+            return Results.NoContent();
+        });
+
+        group.MapDelete("/{id}", async Task<Results<NoContent, NotFound>> (int id, TodoDb db) =>
+        {
+            if (await db.Todos.FindAsync(id) is Todo todo)
+            {
+                db.Todos.Remove(todo);
+                await db.SaveChangesAsync();
+                return TypedResults.NoContent();
+            }
+
+            return TypedResults.NotFound();
+        });
+
+        return group;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,6 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<TodoDb>(opt => opt.UseInMemoryDatabase("TodoList"));
 //  This page is enabled only in the Development environment. 
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+ 
 // The developer exception page is enabled by default in the development environment for minimal API apps
 // https://learn.microsoft.com/en-us/aspnet/core/tutorials/min-web-api?view=aspnetcore-8.0&amp%3Btabs=visual-studio&tabs=visual-studio
 
@@ -13,78 +14,22 @@ builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 //     options.SerializerOptions.WriteIndented = true;
 //     // options.SerializerOptions.IncludeFields = true;
 // });
-var app = builder.Build();if (!app.Environment.IsDevelopment())
+var app = builder.Build();
+if (!app.Environment.IsDevelopment())
 {
+    app.UseExceptionHandler("/Error");
     app.UseStatusCodePages();
 }
+app.Logger.LogInformation($"Application Name: {builder.Environment.ApplicationName}");
+app.Logger.LogInformation($"Environment Name: {builder.Environment.EnvironmentName}");
+app.Logger.LogInformation($"ContentRoot Path: {builder.Environment.ContentRootPath}");
+app.Logger.LogInformation($"WebRootPath: {builder.Environment.WebRootPath}");
 
-
-var todoItems = app.MapGroup("/todoitems");
+app.MapGroup("/todoitems")
+    .MapTodoApi()
+    .WithTags("Public");
 
 app.MapGet("/", () => "Welcome to the Todo API");
-
 app.MapGet("/throw", () => new InvalidOperationException("Sample exception.")); 
 
-todoItems.MapGet("/", GetAllTodos);
-
-todoItems.MapGet("/complete", GetCompleteTodos);
-
-todoItems.MapGet("/{id}", async Task<Results<Ok<TodoItemDTO>, NotFound>> (int Id, TodoDb db) =>
-    await db.Todos.FindAsync(Id)
-        is Todo todo
-            ? TypedResults.Ok(new TodoItemDTO(todo))
-            : TypedResults.NotFound()
-);
-
-todoItems.MapPost("/", async Task<IResult> (TodoItemDTO todoItemDTO, TodoDb db) =>
-{
-    var todo = new Todo{
-        Name = todoItemDTO.Name,
-        IsComplete = todoItemDTO.IsComplete
-    };
-
-    db.Todos.Add(todo);
-    await db.SaveChangesAsync();
-
-    return Results.Created($"/todoitems/{todo.Id}", todo);
-});
-
-todoItems.MapPut("/{id}", async Task<IResult> (int id, TodoItemDTO todoItemDTO, TodoDb db) =>
-{
-    var todo = await db.Todos.FindAsync(id);
-    if (todo is null) return Results.NotFound();
-
-    todo.Name = todoItemDTO.Name;
-    todo.IsComplete = todoItemDTO.IsComplete;
-
-    await db.SaveChangesAsync();
-
-    return Results.NoContent();
-});
-
-todoItems.MapDelete("/{id}", async Task<Results<NoContent, NotFound>> (int id, TodoDb db) =>
-{
-    if (await db.Todos.FindAsync(id) is Todo todo)
-    {
-        db.Todos.Remove(todo);
-        await db.SaveChangesAsync();
-        return TypedResults.NoContent();
-    }
-
-    return TypedResults.NotFound();
-});
-
 app.Run();
-
-static async Task<IResult> GetAllTodos(TodoDb db)
-{
-    return TypedResults.Ok(await db.Todos.Select(t => new TodoItemDTO(t)).ToArrayAsync());
-}
-
-static async Task<IResult> GetCompleteTodos(TodoDb db)
-{
-    return TypedResults.Ok(
-        await db.Todos
-        .Where(t => t.IsComplete)
-        .Select(t => new TodoItemDTO(t)).ToArrayAsync());
-}
